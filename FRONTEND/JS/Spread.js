cf69620177e1cc28bbf78818f9c6aaef5cd62d19
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-04-27 08:46:47 +0300

    update

--- FRONTEND/JS/Spread.js
+++ FRONTEND/JS/Spread.js
@@ -1,4 +1,8 @@
 const first = [1 , 2 ,3];
 const second = [4,5 ,6];
-const combined =[...first ,...second];
+const  combined =first.concat(second);
 console.log(combined);
+let  ombined =[...first ,...second];
+console.log(combined);
+ const clone ={...first};
+console.log(clone);
