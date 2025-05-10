197c5ec201eb30c46417474532f78a6823e636d1
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-05-10 15:48:14 +0300

    update

--- FRONTEND/JS/ES6/index.js
+++
@@ -1,5 +0,0 @@
-import {PI ,getcircumfrence ,getarea} from './math_util.js';
-console.log(PI);
-
-console.log(getcircumfrence(7));
-console.log(getarea(7));
--- FRONTEND/JS/ES6/math_util.js
+++
@@ -1,7 +0,0 @@
-export const PI =3.14159;
-export function getcircumfrence(radius){
-    return 2*PI*radius;
-}
-export function getarea(radius){
-    return radius*PI*radius;
-}
